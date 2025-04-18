a0a472110d2840ddcdc777844bfa4f55fd021570
Samantha Yengst <samantha.yengst@gmail.com>
2025-04-18 11:34:50 -0500

    Added user class

--- sql_test.py
+++ sql_test.py
@@ -54,12 +54,12 @@ def attempt_login(nm, pword):
 
 
 # Globals ??
-p1 = "not set"
+p_login = "not set"
 p2 = "not set"
 p3 = "not set"  
 
-def display_p1():
-    p1.show()
+def display_p_login():
+    p_login.show()
 
 def display_p2():
     p2.show()
@@ -73,8 +73,8 @@ class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
         tk.Frame.__init__(self, *args, **kwargs) # call to parent class
         
-        p1 = Login(self)
-        page1 = p1
+        global p_login
+        p_login = Login(self)
 
         global p2
         p2 = Page2(self)
@@ -87,20 +87,11 @@ class MainView(tk.Frame):
         buttonframe.pack(side="top", fill="x", expand=False)
         container.pack(side="top", fill="both", expand=True)
 
-        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p_login.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
-        # b1 = tk.Button(buttonframe, text="Page 1", command=p1.show)
-        # b2 = tk.Button(buttonframe, text="Page 2", command=p2.show)
-        # b3 = tk.Button(buttonframe, text="Page 3", command=p3.show)
-
-
-        # b1.pack(side="left")
-        # b2.pack(side="left")
-        # b3.pack(side="left")
-
-        p1.show()
+        p_login.show() # shows p1 by deafult
         # p2.show()
         # p3.show()
 
