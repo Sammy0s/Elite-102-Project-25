25ae236f02dff3635c59899d5c491957f907a1f6
Samantha Yengst <samantha.yengst@gmail.com>
2025-04-20 14:49:17 -0500

    Dealing with global cur user bugs sob

--- sql_test.py
+++ sql_test.py
@@ -6,6 +6,7 @@ import tkinter as tk
 class user():
     u_id = -1
     u_lastname = "not set"
+    u_firstname = "not set"
     u_password = "not set"
     u_balance = -1
     u_email = "not set"
@@ -13,14 +14,31 @@ class user():
     def __init__(self, u_tuple):
         self.u_id = u_tuple[0]
         self.u_lastname = u_tuple[1]
-        self.u_password = u_tuple[2]
-        self.u_balance = u_tuple[3]
-        self.u_email = u_tuple[4]
-    
-
+        self.u_firstname = u_tuple[2]
+        self.u_password = u_tuple[3]
+        self.u_balance = u_tuple[4]
+        self.u_email = u_tuple[5]
     
     def __str__(self):
         return f"User: lastname: {self.u_lastname}, acc id: {self.u_id}"
+
+
+
+
+def test_user_object(user_obj, user_id):
+        mycursor.execute(f"Select * from accounts where id = {user_id}")
+        global test_user
+        test_user = None
+        for x in mycursor:
+            print(x)
+            test_user = user(x)
+        
+        if("first names are equal: " + str(user_obj.u_firstname == test_user.u_firstname)):
+            if("last names are equal: " + str(user_obj.u_lastname == test_user.u_lastname)):
+                if("passwords are equal: " + str(user_obj.u_password == test_user.u_password)):
+                    return True
+        return False 
+
 
 
 #### ~~~~~~~~~~~~SQL Backend stuff~~~~~~~~~~~~~~~~
@@ -61,11 +79,14 @@ def select_user(u_lname, u_pass):
 
     if (accs_found > 1):
         print("Error- Multiple accounts found. Duplicate accounts?")
+        return False
     elif (accs_found == 1):
         global cur_user
         cur_user = user(u_tuple)
         print(cur_user)
+        print("test cur_user: " + str(test_user_object(cur_user, 6)))
         return cur_user
+    return False
 
 
     # Okay- at this point I want to give some type of response
@@ -75,14 +96,31 @@ def select_user(u_lname, u_pass):
             # *display bank account details*
 
 
+# Globals ??
+cur_user = user((-1, "not_set", "not_set", "not_set", -1, "not_set"))
+p_login = "not set"
+p_dash = "not set"
+p3 = "not set"  
+
+def display_p_login():
+    p_login.show()
+
+def display_u_dashboard():
+    p_dash.show()
+
+def display_p3():
+    p3.show()
+
+
+
+
+print("test cur_user: " + str(test_user_object(cur_user, 6)))
 
 
 
 
 
-
-
-# For the template on how to use different pages with buttons using the pace and lift functions
+# For the template on how to use different pages with buttons using the place and lift functions
 # https://stackoverflow.com/questions/14817210/using-buttons-in-tkinter-to-navigate-to-different-pages-of-the-application
 # Thank you, Bryan Oakley!!!
 
@@ -116,10 +154,27 @@ class Login(Page):
         u_lname.insert(0, "Last Name")
         u_pass.insert(0, "Password")
 
-class Page2(Page):
+class u_dashboard(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
-       label = tk.Label(self, text="This is page 2")
+       label = tk.Label(self, text="This is the user dashboard")
+       wel = tk.Label(self, text=f"Welcome back, {cur_user.u_firstname}!!")
+       dis_balance = tk.Label(self, pady=10, text=f"Your current balance is: {cur_user.u_balance}.")
+       # dis_rec_trans = tk.Label(self, text=f"Your most recent transaction: \n {cur_user.get_rec_trans()}") TODO most recent transactions require transactions database
+       button_deposit = tk.Button(self, text="Deposit Money", command = lambda: print("Deposit Money Button Pressed"))
+       button_withdrawl = tk.Button(self, text="Withdrawl Money", command = lambda: print("Withdrawl Money Button Pressed"))
+
+       button_logout = tk.Button(self, text="Logout to sign in screen", command = lambda: print("Logout Button Pressed"))
+
+
+       wel.pack()
+       dis_balance.pack()
+
+       button_deposit.pack()
+       button_withdrawl.pack()
+
+       button_logout.pack()
+
        label.pack(side="top", fill="both", expand=True)
 
 class Page3(Page):
@@ -132,25 +187,10 @@ def attempt_login(nm, pword):
     print("yay.")
     print(f"Last name: {nm}, Password: {pword}")
     if (select_user(nm, pword) != None):
-        p2.show()
+        #TODO set cur_user to user that just logged in
+
+        p_dash.show()
     
-
-
-# Globals ??
-cur_user = user((-1, "not_set", "not_set", "not_set", -1, "not_set"))
-p_login = "not set"
-p2 = "not set"
-p3 = "not set"  
-
-def display_p_login():
-    p_login.show()
-
-def display_p2():
-    p2.show()
-
-def display_p3():
-    p3.show()
-
 
 # ~~~~~~~~~~~~~~~~~~~~
 
@@ -161,8 +201,8 @@ class MainView(tk.Frame):
         global p_login
         p_login = Login(self)
 
-        global p2
-        p2 = Page2(self)
+        global p_dash
+        p_dash = u_dashboard(self)
 
         global p3
         p3 = Page3(self)
@@ -175,7 +215,7 @@ class MainView(tk.Frame):
         container.pack(side="top", fill="both", expand=True)
 
         p_login.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
-        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p_dash.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
         p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
 
         p_login.show() # shows p1 by deafult
@@ -187,12 +227,36 @@ class MainView(tk.Frame):
 # TESTING
 
 
+import unittest
+
+class TestUserClass(unittest.TestCase):
+    
+
+    def test_new_user_object(self):
+        mycursor.execute("Select * from accounts where namelast = 'Mendez-Tigre'")
+        global aldair
+        aldair = None
+        for x in mycursor:
+            print(x)
+            aldair = user(x)
+        self.assertEqual(aldair.u_firstname, "Aldair")
+        self.assertEqual(aldair.u_lastname, "Mendez-Tigre")
+        self.assertEqual(aldair.u_password, "123bird")
+
+    
+
+
+# if __name__ == '__main__':
+#     unittest.main()
+     
+
+print("TestUserObj Test Success?: " + str(test_user_object(cur_user, 6)))
 #~~~~~
 # testing connection to MySQL
 
-mycursor.execute("SELECT * from accounts")
-for x in mycursor:
-    print(x)
+# mycursor.execute("SELECT * from accounts")
+# for x in mycursor:
+#     print(x)
 #~~~~~
 
 
@@ -217,7 +281,7 @@ for x in mycursor:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Sam's Bank of Elite 102 tm")
+    root.title("Sam's Bank of Elite 102 (trademarked)")
     main = MainView(root)
     main.pack(side="top", fill="both", expand=True)
     root.wm_geometry("400x400")
